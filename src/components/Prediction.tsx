856835c81c77a8959ee0cea74aa7d89cbf1ff1d4
Reza26-C <dimasmirza22@students.unnes.ac.id>
2026-01-09 11:50:51 +0700

    Update Prediction.tsx

--- src/components/Prediction.tsx
+++ src/components/Prediction.tsx
@@ -105,10 +105,10 @@ const Prediction = () => {
         <div className="container mx-auto px-4">
         <div className="text-center mb-12">
           <h2 className="text-4xl lg:text-5xl font-bold text-foreground mb-4">
-            Cek Status Gizi
+            Cek Stunting
           </h2>
           <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
-            Perhitungan estimasi pertumbuhan anak berdasarkan data fisik.
+            Memprediksi stunting anak berdasarkan data fisik.
           </p>
         </div>
 
